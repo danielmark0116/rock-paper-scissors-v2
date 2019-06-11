766c65b4ac6a7f52424157668d2024e4cd3a38ce
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-06-12 00:12:12 +0200

    Finish summary modal with progress table

--- js/script.js
+++ js/script.js
@@ -2,45 +2,55 @@
 
 // GAME SETTINGS
 
-var btnsTimeout = 1000;
+const btnsTimeout = 1000;
 
+const gameField = document.querySelector('#game-winner');
+const playerScoreField = document.querySelector('#player-score');
+const robotScoreField = document.querySelector('#robot-score');
+const roundCountField = document.querySelector('#round-count');
+const gameSection = document.querySelector('#game-section');
+const resultsSection = document.querySelector('#results-section');
+const startGameBtn = document.querySelector('#start-btn');
+const winnerField = document.querySelector('#winner');
+let maxRoundsCountField = document.querySelector('#max-rounds-count');
+const gameBtns = document.querySelectorAll('.btn-game');
 
-var gameField = document.querySelector("#game-winner");
-var playerScoreField = document.querySelector("#player-score");
-var robotScoreField = document.querySelector("#robot-score");
-var roundCountField = document.querySelector("#round-count");
-var gameSection = document.querySelector("#game-section");
-var resultsSection = document.querySelector("#results-section");
-var startGameBtn = document.querySelector("#start-btn");
-var winnerField = document.querySelector("#winner");
-var maxRoundsCountField = document.querySelector("#max-rounds-count");
-var gameBtns = document.querySelectorAll(".btn-game");
-
-
-var playerScore = 0;
-var robotScore = 0;
-var roundCount = 1;
-var maxRoundsCount;
+// let playerScore = 0;
+// let robotScore = 0;
+// let roundCount = 1;
+// let maxRoundsCount;
 roundCountField.innerHTML = 1;
 
+const params = {
+  playerScore: 0,
+  robotScore: 0,
+  roundCount: 1,
+  maxRoundsCount: 0,
+  progress: []
+};
 
-startGameBtn.addEventListener("click", function () {
-    maxRoundsCount = prompt("How many rounds you want to play?", "Input must be integer");
-    maxRoundsCount = parseInt(maxRoundsCount);
-    maxRoundsCount++;
-    maxRoundsCountField.innerHTML = maxRoundsCount - 1;
-    if (Number.isInteger(maxRoundsCount)) {
-        gameSection.style.display = "block";
-        startGameBtn.classList.toggle("active");
-    } else {
-        alert("Please, type in integer");
-    }
+startGameBtn.addEventListener('click', function() {
+  params.maxRoundsCount = prompt(
+    'How many rounds you want to play?',
+    'Input must be integer'
+  );
 
+  params.maxRoundsCount = parseInt(params.maxRoundsCount);
+  params.maxRoundsCount++;
+
+  maxRoundsCountField.innerHTML = params.maxRoundsCount - 1;
+  let maxRoundsCount = params.maxRoundsCount;
+  if (Number.isInteger(maxRoundsCount)) {
+    gameSection.style.display = 'block';
+    startGameBtn.classList.toggle('active');
+  } else {
+    alert('Please, type in integer');
+  }
 });
 
-// ------------------------------------------------------ 
+// ------------------------------------------------------
 // RULES: ------------------------------------------
-// ------------------------------------------------------ 
+// ------------------------------------------------------
 
 /*
 ROCK PAPER SCISSORS
@@ -50,170 +60,202 @@ ROCK PAPER SCISSORS
 3 - Scissors
 */
 
-
 // FUNCTIONS: ------------------------------------------
 
+// HELPER FUNCTION -> changing value into move
+function numberToMove(attr) {
+  switch (attr) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+    default:
+      return attr;
+  }
+}
+
 // generates random integer - range <0, x>
-var randomChar = x => {
-    return Math.round(Math.random() * (x - 1)) + 1;
+const randomChar = x => {
+  return Math.round(Math.random() * (x - 1)) + 1;
 };
 
 // evaluates who wins, player or robot or both
-var whoWins = (x, y) => {
-    if (x === y) {
-        return "draw";
-    } else if ((x === 1 && y === 3) || (x === 2 && y === 1) || (x === 3 && y === 2)) {
-        playerScore++;
-        playerScoreField.innerHTML = playerScore;
-        scoreAnimation(playerScoreField);
-        return "player";
-    } else if ((x === 1 && y === 2) || (x === 2 && y === 3)) {
-        robotScore++;
-        robotScoreField.innerHTML = robotScore;
-        scoreAnimation(robotScoreField);
-        return "robot";
-    } else {
-        robotScore++;
-        robotScoreField.innerHTML = robotScore;
-        scoreAnimation(robotScoreField);
-        return "robot";
-    }
+const whoWins = (x, y) => {
+  console.log(`player ${x} -------  robot ${y}`);
+  if (
+    (x === 'rock' && y === 1) ||
+    (x === 'paper' && y === 2) ||
+    (x === 'scissors' && y === 3)
+  ) {
+    let whoWins = 'draw';
+    params.progress.push({
+      rndNo: params.roundCount,
+      playerMove: x,
+      robotMove: y,
+      whoWins: whoWins,
+      playerPoints: params.playerScore,
+      robotPoints: params.robotScore
+    });
+    return 'draw';
+  } else if (
+    (x === 'rock' && y === 3) ||
+    (x === 'paper' && y === 1) ||
+    (x === 'scissors' && y === 2)
+  ) {
+    params.playerScore++;
+    playerScoreField.innerHTML = params.playerScore;
+    scoreAnimation(playerScoreField);
+    let whoWins = 'player';
+    params.progress.push({
+      rndNo: params.roundCount,
+      playerMove: x,
+      robotMove: y,
+      whoWins: whoWins,
+      playerPoints: params.playerScore,
+      robotPoints: params.robotScore
+    });
+    return 'player';
+  } else if ((x === 'rock' && y === 2) || (x === 'paper' && y === 3)) {
+    params.robotScore++;
+    robotScoreField.innerHTML = params.robotScore;
+    scoreAnimation(robotScoreField);
+    let whoWins = 'robot';
+    params.progress.push({
+      rndNo: params.roundCount,
+      playerMove: x,
+      robotMove: y,
+      whoWins: whoWins,
+      playerPoints: params.playerScore,
+      robotPoints: params.robotScore
+    });
+    return 'robot';
+  } else {
+    params.robotScore++;
+    robotScoreField.innerHTML = params.robotScore;
+    scoreAnimation(robotScoreField);
+    let whoWins = 'robot';
+    params.progress.push({
+      rndNo: params.roundCount,
+      playerMove: x,
+      robotMove: y,
+      whoWins: whoWins,
+      playerPoints: params.playerScore,
+      robotPoints: params.robotScore
+    });
+    return 'robot';
+  }
 };
 
 // shows the robot's move
-var aiMoveShow = x => {
-    var aiBtns = document.querySelectorAll(".btn-game-robot");
-    aiBtns.forEach(x => {
-        x.classList.remove("active");
-    });
-    aiBtns[x - 1].classList.add("active");
+const aiMoveShow = x => {
+  const aiBtns = document.querySelectorAll('.btn-game-robot');
+  aiBtns.forEach(x => {
+    x.classList.remove('active');
+  });
+  aiBtns[x - 1].classList.add('active');
 };
 
 // ANIMATIONS
-var gameAnimation = x => {
-    var container = document.querySelector("#robot-move");
-    setTimeout(function () {
-        container.classList.add("animated", "bounce");
-    }, 150);
-    setTimeout(function () {
-        gameField.classList.add("animated", "tada");
-        gameField.style.display = "block"
-    }, 50);
-    gameField.style.display = "none";
-    container.classList.remove("animated", "bounce");
-    gameField.classList.remove("animated", "tada");
+const gameAnimation = x => {
+  const container = document.querySelector('#robot-move');
+  setTimeout(function() {
+    container.classList.add('animated', 'bounce');
+  }, 150);
+  setTimeout(function() {
+    gameField.classList.add('animated', 'tada');
+    gameField.style.display = 'block';
+  }, 50);
+  gameField.style.display = 'none';
+  container.classList.remove('animated', 'bounce');
+  gameField.classList.remove('animated', 'tada');
 };
 
-var scoreAnimation = x => {
-
-    setTimeout(function () {
-        x.classList.add("animated", "bounce");
-    }, 150);
-    setTimeout(function () {
-        x.classList.add("animated", "rotateIn");
-        x.style.display = "block"
-    }, 50);
-    x.style.display = "none";
-    x.classList.remove("animated", "bounce");
-    x.classList.remove("animated", "rotateIn");
+const scoreAnimation = x => {
+  setTimeout(function() {
+    x.classList.add('animated', 'bounce');
+  }, 150);
+  setTimeout(function() {
+    x.classList.add('animated', 'rotateIn');
+    x.style.display = 'block';
+  }, 50);
+  x.style.display = 'none';
+  x.classList.remove('animated', 'bounce');
+  x.classList.remove('animated', 'rotateIn');
 };
 
 // MAX round count and then:
-var gameFinish = (maxRounds, roundNumber) => {
-    if (maxRounds === roundNumber) {
-        maxRoundsCountField.innerHTML = "Game has ended";
-        maxRoundsCountField.style.color = "red";
-        setTimeout(x => {
-            var test = document.querySelector("#test");
-            var test2 = document.querySelector("#test2");
-            test.style.display = "none";
-            test2.style.display = "none";
-        }, 1800);
-        roundCountField.style.display = "none";
-        playerRock.classList.toggle("off");
-        playerPaper.classList.toggle("off");
-        playerScissors.classList.toggle("off");
-        setTimeout(x => {
-            resultsSection.style.display = "block";
-        }, 2000);
+const gameFinish = (maxRounds, roundNumber) => {
+  if (maxRounds === roundNumber) {
+    maxRoundsCountField.innerHTML = 'Game has ended';
+    maxRoundsCountField.style.color = 'red';
+    setTimeout(x => {
+      let test = document.querySelector('#test');
+      let test2 = document.querySelector('#test2');
+      test.style.display = 'none';
+      test2.style.display = 'none';
+    }, 1800);
+    roundCountField.style.display = 'none';
+    gameBtns.forEach(x => {
+      x.classList.toggle('off');
+    });
+    setTimeout(x => {
+      resultsSection.style.display = 'block';
+      document.querySelector('#modal').classList.add('show');
+    }, 2000);
 
-
-
-        var winner;
-        if (playerScore === robotScore) {
-            winner = "draw";
-        } else if (playerScore > robotScore) {
-            winner = "player";
-        } else {
-            winner = "robot";
-        }
-        winnerField.innerHTML = winner;
-
-
+    let winner;
+    if (params.playerScore === params.robotScore) {
+      winner = 'draw';
+    } else if (params.playerScore > params.robotScore) {
+      winner = 'player';
+    } else {
+      winner = 'robot';
     }
+    winnerField.innerHTML = winner;
+    params.progress.forEach(x => {
+      let text = document.createElement('tr');
+      text.insertAdjacentHTML(
+        'beforeend',
+        `
+      <th scope="row">${x.rndNo}</th>
+      <td>${x.playerMove}</td>
+      <td>${numberToMove(x.robotMove)}</td>
+      <td>${x.whoWins}</td>
+      <td>${x.playerPoints} - ${x.robotPoints}</td>
+      `
+      );
+      document.querySelector('#results-table').appendChild(text);
+    });
+    console.log(params.progress);
+  }
 };
-
 
 // CODE: ------------------------------------------
 
+// !!!!!!!! CODE CHANGE FOR V2 of the GAME
 
-var playerRock = document.querySelector("#rock");
-var playerPaper = document.querySelector("#paper");
-var playerScissors = document.querySelector("#scissors");
-
-
-
-playerRock.addEventListener('click', function () {
+function playerMove(attr) {
+  gameBtns.forEach(x => {
+    x.classList.toggle('block');
+  });
+  let aiMove = randomChar(3);
+  gameField.innerHTML = whoWins(attr, aiMove);
+  aiMoveShow(aiMove);
+  gameAnimation();
+  params.roundCount++;
+  roundCountField.innerHTML = params.roundCount;
+  gameFinish(params.maxRoundsCount, params.roundCount);
+  setTimeout(x => {
     gameBtns.forEach(x => {
-        x.classList.toggle("block");
+      x.classList.toggle('block');
     });
-    var aiMove = randomChar(3);
-    aiMoveShow(aiMove);
-    gameField.innerHTML = whoWins(1, aiMove);
-    gameAnimation();
-    roundCount++;
-    roundCountField.innerHTML = roundCount;
-    gameFinish(maxRoundsCount, roundCount);
-    setTimeout(x => {
-        gameBtns.forEach(x => {
-            x.classList.toggle("block");
-        });
-    }, btnsTimeout);
+  }, btnsTimeout);
+}
 
-});
-
-playerPaper.addEventListener('click', function () {
-    gameBtns.forEach(x => {
-        x.classList.toggle("block");
-    });
-    var aiMove = randomChar(3);
-    aiMoveShow(aiMove);
-    gameField.innerHTML = whoWins(2, aiMove);
-    gameAnimation();
-    roundCount++;
-    roundCountField.innerHTML = roundCount;
-    gameFinish(maxRoundsCount, roundCount);
-    setTimeout(x => {
-        gameBtns.forEach(x => {
-            x.classList.toggle("block");
-        });
-    }, btnsTimeout);
-});
-
-playerScissors.addEventListener('click', function () {
-    gameBtns.forEach(x => {
-        x.classList.toggle("block");
-    });
-    var aiMove = randomChar(3);
-    aiMoveShow(aiMove);
-    gameField.innerHTML = whoWins(3, aiMove);
-    gameAnimation();
-    roundCount++;
-    roundCountField.innerHTML = roundCount;
-    gameFinish(maxRoundsCount, roundCount);
-    setTimeout(x => {
-        gameBtns.forEach(x => {
-            x.classList.toggle("block");
-        });
-    }, btnsTimeout);
+gameBtns.forEach(x => {
+  x.addEventListener('click', function() {
+    playerMove(this.getAttribute('data-move'));
+  });
 });
